e0476891af555514a98b636f519db4f9666a2758
Adithya-hv <adithyahv04@gmail.com>
2025-02-28 21:06:39 -0500

    Init SC Midtern

---
+++ CPS362_SC/ldu.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // This is our 3x3 matrix A
+    double A[3][3] = {
+        {2.0,  3.0,  1.0},
+        {4.0,  7.0,  2.0},
+        {6.0, 18.0, -1.0}
+    };
+
+}
